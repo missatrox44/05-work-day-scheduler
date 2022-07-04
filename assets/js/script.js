3f88025de562c4200c8d22f0e60c2f821598ee5c
Sara Baqla <missatrox44@gmail.com>
2022-07-04 13:49:53 -0500

    checkTime bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
-//Display current time in Jumbotron (moment)
+//Display current time in Jumbotron 
 var today = moment();
 $("#currentDay").text(today.format('MMMM Do YYYY, h:mm:ss a'));
 
-//create timeblocks from 9am-5pm in hour incrememnts
-//make middle column editable
 
+//compare time
 //make save column clickable/save middle column data to local storage
 //refresh page and data is still displayed
 
@@ -20,18 +19,37 @@ $("#currentDay").text(today.format('MMMM Do YYYY, h:mm:ss a'));
 
 //if time in first column has NOT passed, class="future"
 
+// function checkTime() {
+//     var currentTime = moment().hour();
+//     console.log(currentTime);
+//     var h9El = $('#9');
+//     //if statement in loop
+//     if (currentTime == h9El){
+//     $('.description').addClass('present')
+//     } else if(currentTime > h9El){
+//         $('.description').addClass('future')
+//     } else {
+//         $('.description').addClass('past')
+//     }
+// }
+
+
+//loop through each timeblock .hour & comapre to currentTime
 function checkTime() {
     var currentTime = moment().hour();
     console.log(currentTime);
-    var h9El = $('#9');
     //if statement in loop
-    if (currentTime == h9El){
+    $('.hour').each(function(i) {
+        if (currentTime === '.hour') {
     $('.description').addClass('present')
-    } else if(currentTime > h9El){
+    } else if(currentTime > '.hour'){
         $('.description').addClass('future')
     } else {
         $('.description').addClass('past')
     }
-}
+    })};
 
-checkTime();
+    checkTime();
+
+// Time IDs as an array
+// var timeIds = ['9', '10', '11', '12', '1', '2', '3', '4', '5']
