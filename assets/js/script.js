cb1c935ffcc12983ed146bc54fafcc10898c6f1d
Sara Baqla <missatrox44@gmail.com>
2022-07-05 18:08:02 -0500

    localstorage bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,13 @@ var today = moment();
 $("#currentDay").text(today.format('MMMM Do YYYY, h:mm:ss a'));
 
 
-//compare time
-//make save column clickable/save middle column data to local storage
-//refresh page and data is still displayed
-
+//compare timeblock to current Time
 var textBox = $('.description').toArray();
-console.log(textBox);
+// console.log(textBox);
 
 //function to change color of textarea based on currentTime
-// console.log(textBox);
 function checkTime() {
     var currentTime = moment().hour();
-    // var textBox = $('.description').toArray();
     console.log(currentTime);
     for (var i = 0; i < textBox.length; i++) {
         if (currentTime === i+9) {
@@ -23,11 +18,23 @@ function checkTime() {
             $(textBox[i]).addClass('future');
         } else {
             $(textBox[i]).addClass('past');
-        }
+          }
     }
 };
 
 checkTime();
 
+//Save user input to local storage
+var saveBtn = $('.saveBtn');
 
-//need to change 
+function saveUserInput (){
+    saveBtn.on('click'), function(){
+        // localStorage.setItem('textBox', textBox.val());
+        localStorage.setItem('textBox', textBox.input);
+    }
+}
+
+saveUserInput();
+
+//refresh page and data is still displayed
+$(textBox.input).val(localStorage.getItem(textBox));
